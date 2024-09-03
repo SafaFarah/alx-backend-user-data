c77595a3a56206a8740a7de77bb7d4d2ed31b9a2
SafaFarah <kangjisook13@gmail.com>
2024-09-03 18:04:36 +0300

    create method def extract_user_credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,3 +41,20 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Extracts  (email and password) from the decoded Base64.
+        Args:
+            decoded_base64_authorization_header:decoded Base64 authorization.
+        Returns:
+tuple: containing the email and password if valid,or (None, None).
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
