511b84707507ffdd50afadc04128921451a31da9
SafaFarah <kangjisook13@gmail.com>
2024-09-03 17:08:41 +0300

    create  class BasicAuth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Basic Authentication class
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic authentication class inheriting from Auth.
+    """
+    pass
