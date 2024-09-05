25bde66029e160ac6462ed4d9e0adc2ab1d4daa0
SafaFarah <kangjisook13@gmail.com>
2024-09-05 21:30:24 +0300

    create class SessionAuth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module for Session Authentication """
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ SessionAuth class that inherits from Auth.
+        Currently empty, to be expanded in later steps.
+    """
+    pass
