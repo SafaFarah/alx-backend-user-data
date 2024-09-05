44774ba7d1268f4cba713f1dea4b14362edd37a0
SafaFarah <kangjisook13@gmail.com>
2024-09-06 02:25:45 +0300

    create def current_user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,9 +25,5 @@ class SessionAuth(Auth):
     def current_user(self, request=None):
         """ Return User instance based on session cookie """
         session_id = self.session_cookie(request)
-        if session_id is None:
-            return None
         user_id = self.user_id_for_session_id(session_id)
-        if user_id is None:
-            return None
         return User.get(user_id)
