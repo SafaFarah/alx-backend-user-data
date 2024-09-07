1f2f05815514d2d8b3f53e1f99905a3776dea833
SafaFarah <kangjisook13@gmail.com>
2024-09-07 20:44:55 +0300

    debug session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ from api.v1.auth.auth import Auth
 import uuid
 from models.user import User
 
+
 class SessionAuth(Auth):
     """ SessionAuth class for managing session-based authentication """
     user_id_by_session_id = {}
