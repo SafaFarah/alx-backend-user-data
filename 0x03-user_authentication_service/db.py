f5e5858764842b8d213c4ae01f532cc731c9b6fc
SafaFarah <kangjisook13@gmail.com>
2024-09-10 17:07:47 +0300

    debug db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -39,9 +39,13 @@ class DB:
         Returns:
             User: The newly created user object.
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            session.rollback()
+            new_user = None
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
