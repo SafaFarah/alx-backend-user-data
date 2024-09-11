35e53eb45b39fe153f4cfa57b6a90916f6797dde
SafaFarah <kangjisook13@gmail.com>
2024-09-12 02:16:57 +0300

    debug update_password function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -91,9 +91,9 @@ def update_password():
         reset_token = request.form.get('reset_token')
         new_password = request.form.get('new_password')
         auth.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
